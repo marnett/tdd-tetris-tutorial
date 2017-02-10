f207efcf9a47819e03647550a37d3ef54affbf8a
Mitchell Arnett <mitchell.arnett@gmail.com>
2017-02-09 20:22:33 -0500

    added I_SHAPE and O_SHAPE constants to Tetromino class

--- src/main/java/tetris/Tetromino.java
+++ src/main/java/tetris/Tetromino.java
@@ -2,31 +2,43 @@ package tetris;
 
 public final class Tetromino  {
 	
-	public static final Tetromino T_SHAPE = new Tetromino();
+	public static final Tetromino T_SHAPE = new Tetromino("" +
+            ".T.\n" +
+            "TTT\n" +
+            "...\n");
 	
-	private final Piece T_Shape;
+	public static final Tetromino I_SHAPE = new Tetromino("" +
+            ".....\n" +
+            ".....\n" +
+            "IIII.\n" +
+            ".....\n" +
+            ".....\n");
 	
-	private Tetromino() {
-		T_Shape = new Piece("" +
-	            ".T.\n" +
-	            "TTT\n" +
-	            "...\n");
+	public static final Tetromino O_SHAPE = new Tetromino("" +
+                    ".OO\n" +
+                    ".OO\n" +
+                    "...\n");
+	
+	private final Piece shape;
+	
+	private Tetromino(String shapeType) {
+		shape = new Piece(shapeType);
 	}
 	
 	private Tetromino(Piece piece) {
-		T_Shape = piece;
+		shape = piece;
 	}
 
 	public Tetromino rotateRight() {
-		return new Tetromino(T_Shape.rotateRight());
+		return new Tetromino(shape.rotateRight());
 	}
 
 	public Tetromino rotateLeft() {
-		return new Tetromino(T_Shape.rotateLeft());
+		return new Tetromino(shape.rotateLeft());
 	}
 	
 	public String toString() {
-		return T_Shape.toString();
+		return shape.toString();
 	}
 	
 }
